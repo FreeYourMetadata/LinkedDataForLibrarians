7c4c4cad35722f7fb56d658686b49a68733fd599
Ruben Verborgh <ruben@verborgh.org>
2017-06-10 11:50:16 +0200

    Add self-assessment script.

--- scripts/enhancements.js
+++ scripts/enhancements.js
@@ -11,5 +11,20 @@ shower.modules.require(['shower'], function (Shower) {
       if (e.classList.contains('cover'))
         e.parentElement.appendChild(document.createElement('div')).classList.add('handle');
     });
+
+    // Activate evaluation slides
+    document.querySelectorAll('.evaluation.slide').forEach(function (slide) {
+      var options = slide.querySelectorAll('ol > li'), selected = null;
+      options.forEach(function (option) {
+        option.addEventListener('click', function () {
+          if (selected)
+            selected.classList.remove('selected');
+          if (selected === option)
+            selected = null;
+          else
+            (selected = option).classList.add('selected');
+        });
+      });
+    });
   });
 });
